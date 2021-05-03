3f172de27c5c6f84d6ec0da32c7e0c1f5f6b5a00
ImGabreuw <imgabreuw.dev@gmail.com>
2021-05-03 15:10:45 -0300

    Project created

---
+++ backend/src/main/java/me/gabreuw/dsvendas/DsvendasApplication.java
@@ -0,0 +1,13 @@
+package me.gabreuw.dsvendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsvendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsvendasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/me/gabreuw/dsvendas/DsvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package me.gabreuw.dsvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
